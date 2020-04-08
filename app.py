1a3f019b0cd13c1063a47ae5864c9bdf8dcc1013
Sekina-Muntaz <sherzmuntaz@gmail.com>
2020-04-08 18:27:41 +0300

    Data Visualization

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 # importing
 from flask import Flask,render_template, request, redirect,url_for
+import pygal
 
 #instanciating a class
 app=Flask(__name__)
@@ -34,12 +35,101 @@ def inventory():
     if request.method=="POST":
         # print('worked')
         name=request.form['name']
+        invType=request.form['invType']
+        sellingPrice=request.form['sellingPrice']
+        buyingPrice=request.form['buyingPrice']
+        
 
         print(name)
+        print(invType)
+        print(sellingPrice)
+        print(buyingPrice)
+    
         return redirect(url_for('inventory'))
 
 
     return render_template('inventory.html')
+@app.route('/addStock', methods=['GET','POST'])
+def addStock():
+
+    # # receive from a form
+    if request.method=="POST":
+        # print('worked')
+        stock=request.form['stock']
+        
+
+        print(stock)
+    
+        return redirect(url_for('inventory'))
+
+
+    return render_template('inventory.html')
+@app.route('/addSale',methods=['POST','GET'])
+def addSale():
+    if request.method=="POST":
+        sale=request.form['sale']
+        
+
+        print(sale)
+       
+
+        return redirect(url_for('inventory'))
+
+
+
+    return render_template('inventory.html')
+@app.route('/editInv', methods=['POST','GET'])
+def editInv():
+    if request.method=='POST':
+        name=request.form['name']
+        invType=request.form['invType']
+        sellingPrice=request.form['sellingPrice']
+        buyingPrice=request.form['buyingPrice']
+
+        print(name)
+        print(invType)
+        print(sellingPrice)
+        print(buyingPrice)
+
+        return redirect(url_for('inventory'))
+    
+    
+    
+    return render_template('inventory.html')
+@app.route('/data_visualization')
+def data_visualization():
+    # initializing pie chart
+    pieChart=pygal.Pie()
+    # add components to piechart
+    pieChart.title="Corona Virus in Kenya"
+    #  Partitioning your pie chart
+    pieChart.add('Mombasa',60)
+    pieChart.add('Nairobi',140)
+    pieChart.add('Kilifi',100)
+    pieChart.add('Kwale',50)
+    pieChart.add('Mombasa',80)
+
+    pieData=pieChart.render_data_uri()
+
+
+    # line graph
+    line_chart = pygal.Line()
+    line_chart.title = 'Browser usage evolution (in %)'
+    line_chart.x_labels = map(str, range(2002, 2013))
+    line_chart.add('Firefox', [None, None,    0, 16.6,   25,   31, 36.4, 45.5, 46.3, 42.8, 37.1])
+    line_chart.add('Chrome',  [None, None, None, None, None, None,    0,  3.9, 10.8, 23.8, 35.3])
+    line_chart.add('IE',      [85.8, 84.6, 84.7, 74.5,   66, 58.6, 54.7, 44.8, 36.2, 26.6, 20.1])
+    line_chart.add('Others',  [14.2, 15.4, 15.3,  8.9,    9, 10.4,  8.9,  5.8,  6.7,  6.8,  7.5])
+
+    lineData=line_chart.render_data_uri()
+
+    
+
+    return render_template('chart.html',pie=pieData,line=lineData)
+
+
+    
+
 
 
 
